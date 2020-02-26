cd6a1742943de8807948910c621c4e511fa735fa
Praszad <silverprasad01@gmail.com>
2020-02-26 21:56:28 +0530

    Login Bootstrap Sample Desin Added

--- src/components/Login/LoginComponent.jsx
+++ src/components/Login/LoginComponent.jsx
@@ -3,7 +3,25 @@ import React from 'react';
 export default function LoginComponent(params) {
   return (
     <div>
-      <h2>Login Component</h2>
+      <div className='container'>
+        <form action=''>
+          <div className='form-group'>
+            <label htmlFor=''>
+              <input type='text' className='form-element' />
+            </label>
+          </div>
+          <div className='form-group'>
+            <label htmlFor=''>
+              <input type='text' className='form-element' />
+            </label>
+          </div>
+          <div className='form-group'>
+            <button type='button' className='btn btn-primary'>
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
